0b6925dc09c6ec89bfe39395ee80ae81fcae6466
System User <alexandre.odoni@dif16.cmc.pr.gov.br>
2019-10-18 15:17:52 -0300

    script de build

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+yarn build
+
+SOURCE_BRANCH="master"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy."
+    exit 0
+fi
+
+cd build
+git init
+
+git config user.name "Travis CI"
+git config user.email "aleodoni@gmail.com"
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
