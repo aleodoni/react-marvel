604ecacf49841c2c0d4881dd6ec1d611fe539cbd
Travis CI <aleodoni@gmail.com>
2019-10-18 20:55:07 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.6d6341debe611e51b22ddc21f1621117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ac637e20677f24dde50299528dde67",
+    "url": "/react-marvel/index.html"
+  },
+  {
+    "revision": "daa7e1cb9f1ea7145fcf",
+    "url": "/react-marvel/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "daa7e1cb9f1ea7145fcf",
+    "url": "/react-marvel/static/js/2.0f2e6880.chunk.js"
+  },
+  {
+    "revision": "e6276343f239813ceea6",
+    "url": "/react-marvel/static/js/main.73efb514.chunk.js"
+  },
+  {
+    "revision": "09a2024793253fad22f4",
+    "url": "/react-marvel/static/js/runtime-main.95e88fdd.js"
+  },
+  {
+    "revision": "cc3a8cb9ea19776093219ce81cc43c49",
+    "url": "/react-marvel/static/media/marvel.cc3a8cb9.jpg"
+  }
+]);
